314554a5468cd9f26e975f364e844803085b55ff
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2017-02-08 02:33:26 +0900

    [WIP] Set interfaces about detail page.

---
+++ BackgroundGradationScroll/ColumnContainerController.swift
@@ -0,0 +1,21 @@
+//
+//  ColumnContainerController.swift
+//  BackgroundGradationScroll
+//
+//  Created by 酒井文也 on 2017/02/08.
+//  Copyright © 2017年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ColumnContainerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ BackgroundGradationScroll/ColumnDetailController.swift
@@ -0,0 +1,21 @@
+//
+//  ColumnDetailController.swift
+//  BackgroundGradationScroll
+//
+//  Created by 酒井文也 on 2017/02/08.
+//  Copyright © 2017年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ColumnDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
